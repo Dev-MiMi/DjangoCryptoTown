01893bd76c2f4d811165c714de6ff0e2f4f073e1
DrMiMI <olagundoyemiracle5@gmail.com>
2025-11-28 14:26:58 +0100

    .

---
+++ crypto_town/config/settings/test.py
@@ -0,0 +1,38 @@
+"""
+With these settings, tests run faster.
+"""
+
+from .base import *  # noqa: F403
+from .base import TEMPLATES
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="4fZ6nRgUCmMg7aRYGKK7JfyBzNSrcqgWw4tM6PF4buy0y05UJkWRRH27rfnl0x9V",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+# PASSWORDS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+# DEBUGGING FOR TEMPLATES
+# ------------------------------------------------------------------------------
+TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore[index]
+
+# MEDIA
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-url
+MEDIA_URL = "http://media.testserver/"
+# Your stuff...
+# ------------------------------------------------------------------------------
